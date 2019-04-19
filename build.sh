ce9b80b7f0d398266ede7e4b4f9229670a20290d
Sylvain Henry <sylvain@haskus.fr>
2019-04-19 18:25:48 +0200

    Require Stack 2.0

--- build.sh
+++
@@ -1,7 +0,0 @@
-(cd ~/repo/ghc/ghc-studio/inplace/bin; ln -s ghc-stage2 ghc)
-export PATH=/home/hsyl20/repo/ghc/ghc-studio/inplace/bin:$PATH
-cabal sandbox init
-cabal configure --allow-newer=base
-cabal install --only-dependencies --allow-newer=base
-cabal build
-
